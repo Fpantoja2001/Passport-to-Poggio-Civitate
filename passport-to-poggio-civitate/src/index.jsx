df7c161ef0d0ba43cfe8b91a0da4fb1231ca6dc7
Fpantoja2001 <121701118+Fpantoja2001@users.noreply.github.com>
2023-09-01 11:56:37 -0400

    project setup

---
+++ passport-to-poggio-civitate/src/index.jsx
@@ -0,0 +1,14 @@
+import './main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+
